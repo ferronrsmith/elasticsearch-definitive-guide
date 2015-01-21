9dad2c25d70c6421d7907f0bd7884fc5a279c850
ferron <ferronrsmith@gmail.com>
2015-01-21 15:12:59 -0400

    added support for epub publishing. more efficient than pdf

--- gen_book.sh
+++ gen_book.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-REPO_PATH="/home/ferron/personal_git/elasticsearch-definitive-guide/images"
+if [ "$1" == "pdf" ]; then
+    REPO_PATH="/home/ferron/personal_git/elasticsearch-definitive-guide/images"
 
-# generate docbook.xml file
-a2x --icons -f docbook -r "$REPO_PATH" -d book book.asciidoc -v
+    # generate docbook.xml file
+    a2x --icons -f docbook -r "$REPO_PATH" -d book book.asciidoc -v
 
-# apply template to book.xml
-xsltproc /usr/share/xml/docbook/stylesheet/docbook-xsl/fo/docbook.xsl book.xml > book.fo
+    # apply template to book.xml
+    xsltproc /usr/share/xml/docbook/stylesheet/docbook-xsl/fo/docbook.xsl book.xml > book.fo
 
-# generate pdf
-fop -fo book.fo book.pdf
+    # generate pdf
+    fop -fo book.fo book.pdf
+elif [ "$1" == "epub" ]; then
+    a2x -fepub -dbook book.asciidoc -v
+else
+    echo "format not supported, choose pdf or epub"
+fi
